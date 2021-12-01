3c2ebfcd98d39c4f186b4434a5d9c6483a5d1c68
unknown <devfrontend01@gmail.com>
2021-11-30 21:48:54 -0300

    uploadImage function fixed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-const containerResizable = document.querySelector('.container__resizable')
-const getWidth = document.querySelector('#width')
-const getHeight = document.querySelector('#height')
+const file = document.querySelector('#uploadFile-btn')
+const chosenImage = document.querySelector('.chosenImage')
+const uploadButtonWrapper = document.getElementById('uploadButtonWrapper')
 
-function getSize(){
-    getWidth.innerHTML = `${containerResizable.clientWidth} px`
-    getHeight.innerHTML = `${containerResizable.clientHeight} px`
+file.addEventListener('change', uploadImage)
+
+function uploadImage(){
+    let picture = this.files[0]
+
+    if(picture){
+        let reader = new FileReader();
+
+        reader.addEventListener('load', ()=>{
+            chosenImage.setAttribute('src', reader.result)
+            uploadButtonWrapper.style.display='none';
+        })
+        reader.readAsDataURL(picture)
+    }
 }
-
