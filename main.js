ca9ff14522dcf1c74575b5179aa423aa972e3e91
Junior Macedo <80720405+JuniorMacedo91@users.noreply.github.com>
2021-12-04 01:12:00 -0300

    downloadPicture and deleteImage functions created

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const file = document.querySelector('#uploadFile-btn')
-const chosenImage = document.querySelector('.chosenImage')
 const uploadButtonWrapper = document.getElementById('uploadButtonWrapper')
-const containerResizable = document.getElementById('container__resizable');
+const containerResizable = document.getElementById('container__resizable')
+const download = document.querySelector('#download-btn')
 const deletePicture = document.querySelector('.fa-trash')
 
 
 file.addEventListener('change', uploadImage)
 containerResizable.addEventListener('mousemove', handleResize)
 containerResizable.addEventListener('touchmove', handleResize)
+download.addEventListener('click', downloadPicture)
 deletePicture.addEventListener('click', deleteImage)
 
 
@@ -18,20 +19,31 @@ function uploadImage(){
         let reader = new FileReader();
 
         reader.addEventListener('load', ()=>{
-            chosenImage.setAttribute('src', reader.result)
+            anchor = document.createElement('a')
+            const image = document.createElement('img')
+
+            anchor.href = reader.result
+            anchor.download = picture.name;
+            image.src = reader.result
+
+            containerResizable.appendChild(anchor)
+            anchor.appendChild(image)
+
         })
         reader.readAsDataURL(picture)
     }
+}
+
+function downloadPicture(){
+    anchor.click()
 }
 
 function handleResize(){
     let width = document.getElementById('width').innerHTML = `${containerResizable.clientWidth} px`;
     let height = document.getElementById('height').innerHTML = `${containerResizable.clientHeight} px`;
 }
-
 handleResize()
 
-
 function deleteImage(){
-    chosenImage.removeAttribute('src')
+    containerResizable.removeChild(anchor)
 }
